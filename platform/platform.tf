f25bbda98bf575259c437a13034c0198d0538639
Nathan Costello <ncostello81@gmail.com>
2021-05-13 16:20:06 +1000

    Added role assignment to allow AKS to pull from ACR.

--- platform/platform.tf
+++ platform/platform.tf
@@ -320,6 +320,12 @@ resource "azurerm_kubernetes_cluster" "aks" {
     }
 }
 
+resource "azurerm_role_assignment" "aks_acr_pull" {
+    scope                = azurerm_container_registry.acr.id
+    role_definition_name = "AcrPull"
+    principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.client_id
+}
+
 output "container_registry_user" {
     value = azurerm_container_registry.acr.admin_username
 }
