732091b956ea5c128707334bd9957aaf359536f4
Nathan Costello <ncostello81@gmail.com>
2021-05-13 16:46:05 +1000

    Fix for kubelet_identity being a collection apparently.

--- platform/platform.tf
+++ platform/platform.tf
@@ -323,7 +323,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_role_assignment" "aks_acr_pull" {
     scope                = azurerm_container_registry.acr.id
     role_definition_name = "AcrPull"
-    principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.client_id
+    principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity.0.client_id
 }
 
 output "container_registry_user" {
